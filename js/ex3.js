c44aacfcf6a1f2bd16596525d8437edf6fff8a68
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-03 14:35:36 -0300

    exercicio 1

---
+++ js/ex3.js
@@ -0,0 +1,36 @@
+// Ex: 03
+
+/*console.log('Exercicio 03');
+
+function celsiusToFahrenheit(temp_c) {
+    var temp_f = (9 * temp_c / 5) + 32;
+    return temp_f;
+};
+
+=> imput = temp_celsius
+=> button = converter
+=> div = temp_fahr
+
+var celsius = parseFloat(document.getElementById("caixa_azul").innerHTML);
+
+var fahrenheit = celsiusToFahrenheit(celsius);
+
+document.getElementById("caixa_amarela").innerHTML = "<strong>" + fahrenheit + "</strong>";*/
+
+console.log('----------Exercicio 03 === nº 01--------');
+
+function celsiusToFahrenheit(temp_c) {
+    var temp_f = (9 * temp_c / 5) + 32;
+    return temp_f;
+};
+
+document.getElementById("converter").onclick = function () {
+    var celsius = parseFloat(document.getElementById("temp_celsius").value);
+    var fahrenheit = celsiusToFahrenheit(celsius);
+    document.getElementById("temp_fahr").innerHTML = "<strong>" + fahrenheit + "</strong>";
+}
+
+//Exercicio 02
+console.log('----------Exercicio 03 === nº 02--------');
+
+// => anos_copa
