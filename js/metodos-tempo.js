f09746b508313d654dcd50207a6c6ab71c3cd771
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-08 17:12:02 -0300

    metodos de tempo

---
+++ js/metodos-tempo.js
@@ -0,0 +1,11 @@
+//  Métodos de Tempo 1º setTimeout aceita dois argumentos: uma função a ser executada após um 
+//determinado tempo e o tempo em milisegundos a esperar.
+
+// Aula 27 - Métodos de Tempo
+
+console.log('Mensagem 1');
+
+//função tempo em milisegundos
+window.setTimeout(function(){
+    console.log('Mensagem 2');
+},3000);
