3d92f6a2c6ee59195cef52695fb1c49dd8033e6f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-10 11:16:10 -0300

    Formularios Mostra os valores

--- js/formula.js
+++ js/formula.js
@@ -11,3 +11,16 @@ document.getElementById("mostrar_opcao").onclick = function () {
 
 //---------------------------------------------------
 
+document.getElementById("mostrar_opcao1").onclick = function () {
+
+    // var campo_select = document.getElementById("options");
+    // var indice_selecionado = campo_select.options.selectedIndex;
+    // var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+    // document.getElementById("opcao_selecionada").innerHTML = valor_selecionado;
+
+    var valor_selecionado = document.getElementById("options1").value;
+    document.getElementById("opcao_selecionada1").innerHTML = valor_selecionado;
+
+};
+
+ //------------------------------------------------
