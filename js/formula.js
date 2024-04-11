68f037772e1009382c593fe8c72c63c7ba4e4b0b
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-11 11:20:51 -0300

    Check Boxes

--- js/formula.js
+++ js/formula.js
@@ -44,3 +44,21 @@ document.getElementById("mostrar_radio").onclick = function () {
     document.getElementById("radio_selecionado").innerHTML = radio_selected;
 
 };
+
+//------------------
+// Check Boxes
+
+document.getElementById("mostrar_check").onclick = function () {
+    document.getElementById("check_selecionado").innerHTML = "";
+
+
+    var check = document.getElementsByName("interesse");
+
+    for (var b = 0; b < check.length; b++) {
+        if (check[b].checked) {
+            document.getElementById("check_selecionado").innerHTML += '<li>' + check[b].value + '</li>';
+        }
+    }
+
+
+};
