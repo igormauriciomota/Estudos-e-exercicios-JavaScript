108acb29a45d4d5d43f80b8cf5b18fce7a92465f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-12 16:20:28 -0300

    pedido data de entrega

--- js/ex4.js
+++ js/ex4.js
@@ -5,5 +5,50 @@ document.getElementById("estados").onchange = function() {
 }
 
 if (localStorage.estado) {
-    document.getElementById("estado").value = localStorage.estado;
+    document.getElementById("estados").value = localStorage.estado;
+}
+
+//--------------
+
+// ex 02
+
+function add_left_zero(number) {
+    if (number < 10) {
+        return "0" + number.toString();
+    } else {
+        return number.toString();
+    }
+}
+
+function format_date(timestamp) {
+    //                            dia
+    var dia = add_left_zero(new Date(timestamp).getDate());
+    //                           mes de 0 a 11
+    var mes = add_left_zero(new Date(timestamp).getMonth() + 1);
+    //                            Ano
+    var ano = add_left_zero(new Date(timestamp).getFullYear());
+    return dia + "-" + mes + "-" + ano;
+}
+
+document.getElementById("confirmar_pedido").onclick = function() {
+
+    var selection = document.getElementById("envios").value;
+
+    if (selection == "escolha") {
+        alert("Escolha um modelo de envio");
+    } else {
+        if (selection == "normal") {
+            var dias_entrega = 18;
+        } else if (selection == "express") {
+            var dias_entrega = 12;
+        }
+
+        var data_envio = new Date().getTime();
+        var data_entrega = data_envio + (dias_entrega * 86400000);
+
+        document.getElementById("data_pedido").innerHTML = format_date(data_envio);
+        document.getElementById("data_entrega").innerHTML = format_date(data_entrega);
+
+    }
+
 }
