192d8e0dfc2d74a8022a12eecfe3df2e5ba601e1
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-17 16:33:53 -0300

    Jquery

---
+++ js/jquery.js
@@ -0,0 +1,29 @@
+// Aula 34 - jQuery - Sintaxe
+// em JS => document.getElementById("exemplo").innerHTML = "Olá";
+// ex: quando for urar os elementos => $("div").ação(); - $("p").ação();
+// ex: class(.) ou id(#) => $("#exemplo").ação();
+//ex: .ação(); = .html("olá"); = .css("");
+
+//$("#exemplo").html("Olá");
+
+/*$("#esconder").click(function(){
+    $(".exemplo").css("display","none"); => .hide(); e none
+});*/
+
+$("#esconder").click(function(){
+    $(".exemplo").hide();
+});
+
+//--------------------------------Aula 35 - JQuery - Manipulação do conteúdo HTML
+
+var conteudo_html = $("#paragrafo-html").html();
+console.log(conteudo_html);
+
+$("#paragrafo-html").html("Mudar o conteudo do paragrafo");
+
+conteudo_html = $("#paragrafo-html").html();
+console.log(conteudo_html);
+
+//----------------------------------------------------
+
+
