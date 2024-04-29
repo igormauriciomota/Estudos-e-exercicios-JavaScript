ac611f93f4fdc6c464449220a5b858b3e45379ff
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-29 16:27:56 -0300

    Erros handling

--- js/js-Intermediario.js
+++ js/js-Intermediario.js
@@ -19,3 +19,52 @@ pegar_usuario(function(user){
     console.log('Olá ' + user.nome + ', como vai?');
 });
 
+//----------------------------------------------------
+
+// Aula 44 - Erros handling
+
+function pegar_usuario(){
+
+    window.setTimeout(function(){
+        var u = {
+            'nome': 'João'
+        };
+        return u;
+    }, 1000);
+    
+}
+        
+function saudar_usuario(user) {
+    console.log('Olá ' + user.nome + ', como vai?');
+}
+
+try {
+// comandos que geraram erro utilizando o comanto (try) e colocando dentro dele para evitar o erro
+var user = pegar_usuario();
+saudar_usuario(user);
+} catch {
+    console.log('Olá, como vai?') ;
+} finally {
+    console.log('Este comando sera sempre executado');
+}
+
+
+console.log('Novo comando para executar ao final');
+
+//---------------------------------Em branco
+
+function saudar_usuario(user) {
+    console.log('Olá ' + user.nome + ', como vai?');
+}
+
+var user = {'nome': ''};
+
+try {
+
+    if (user.nome == '') {
+        throw 'Nome em Branco';
+    }
+
+} catch(err) {
+    console.log(err);
+}
