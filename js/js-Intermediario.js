2177dcd842aa8323fa908e981d61cf97bbda0218
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-05-02 20:25:37 -0300

    Ajax

--- js/js-Intermediario.js
+++ js/js-Intermediario.js
@@ -227,3 +227,24 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
 xhttp.send();
 
+// Aula 51-Ajax Response
+
+function mostrar_temperatura(dados) {
+    var dados_obj = JSON.parse(dados);
+    console.log('A temperatura em Londres é de ' + dados_obj.main.temp + ' graus Celsius.');
+}
+
+function tempo_londres(callback) {
+    var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+                    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+            
+        }
+    };
+    xhttp.open("GET", "https://openweathermap.org/data/2.5/weather?q=London,8be1aae68be7f754a864aa5026fb84c7");
+    xhttp.send();
+}
+
+tempo_londres(mostrar_temperatura);
